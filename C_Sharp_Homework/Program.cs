b708499f712351abe7a3e3c7fe15e6cf606c99d4
Бауыржан <@->
2019-01-07 02:19:53 +0600

    Добавьте файлы проекта.

---
+++ C_Sharp_Homework/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Homework
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TestClass.Method1();
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"Catch в Main : {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Блок finally в Main");
+            }
+            Console.WriteLine("Конец метода Main");
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
+
